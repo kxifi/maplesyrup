3dd01c8c7a68625ce27d83b40d1998a122229d67
K. <kai@localhost>
2025-10-20 11:22:58 +0600

    init?

---
+++ casks/maples.rb
@@ -0,0 +1,11 @@
+cask "maples" do
+  version "0.0.1"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://example.com/maples.dmg"
+  name "Maples"
+  desc "Placeholder for Maples GUI app"
+  homepage "https://example.com"
+
+  app "Maples.app"
+end
